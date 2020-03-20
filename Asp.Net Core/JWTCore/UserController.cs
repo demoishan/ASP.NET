8c3a7c61258f2b6d344bedecb73793bc34aeb560
demoishan <32594322+demoishan@users.noreply.github.com>
2020-03-20 12:04:55 +0530

    Update UserController.cs

--- Asp.Net Core/JWTCore/UserController.cs
+++ Asp.Net Core/JWTCore/UserController.cs
@@ -13,7 +13,8 @@ namespace AuthUsingJWT.Controllers
     public class UserController : ControllerBase
     {
         [HttpGet]
-        //[Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin")]
+        [Route("Get1")]
         public ActionResult<IEnumerable<string>> Get()
         {
             var currentUser = HttpContext.User;
@@ -36,8 +37,107 @@ namespace AuthUsingJWT.Controllers
         }
 
         [HttpGet]
-        //[Authorize(Roles = "SuperAdminAdmin")]
-        public ActionResult<IEnumerable<string>> GetByID()
+        [Route("Get2")]
+        [Authorize(Roles = "SuperAdmin")]
+        public ActionResult<IEnumerable<string>> Get2()
+        {
+            var currentUser = HttpContext.User;
+            int spendingTimeWithCompany = 0;
+
+            if (currentUser.HasClaim(c => c.Type == "DateOfJoing"))
+            {
+                DateTime date = DateTime.Parse(currentUser.Claims.FirstOrDefault(c => c.Type == "DateOfJoing").Value);
+                spendingTimeWithCompany = DateTime.Today.Year - date.Year;
+            }
+
+            if (spendingTimeWithCompany > 5)
+            {
+                return new string[] { "High Time1", "High Time2", "High Time3", "High Time4", "High Time5" };
+            }
+            else
+            {
+                return new string[] { "value1", "value2", "value3", "value4", "value5" };
+            }
+        }
+
+        [HttpGet]
+        [Route("Get3")]
+        [Authorize(Roles = "SuperAdmin,User")]
+
+        public ActionResult<IEnumerable<string>> Get3()
+        {
+            var currentUser = HttpContext.User;
+            int spendingTimeWithCompany = 0;
+
+            if (currentUser.HasClaim(c => c.Type == "DateOfJoing"))
+            {
+                DateTime date = DateTime.Parse(currentUser.Claims.FirstOrDefault(c => c.Type == "DateOfJoing").Value);
+                spendingTimeWithCompany = DateTime.Today.Year - date.Year;
+            }
+
+            if (spendingTimeWithCompany > 5)
+            {
+                return new string[] { "High Time1", "High Time2", "High Time3", "High Time4", "High Time5" };
+            }
+            else
+            {
+                return new string[] { "value1", "value2", "value3", "value4", "value5" };
+            }
+        }
+
+        [HttpGet]
+        [Route("Get4")]
+        [Authorize(Roles = "User")]
+
+        public ActionResult<IEnumerable<string>> Get4()
+        {
+            var currentUser = HttpContext.User;
+            int spendingTimeWithCompany = 0;
+
+            if (currentUser.HasClaim(c => c.Type == "DateOfJoing"))
+            {
+                DateTime date = DateTime.Parse(currentUser.Claims.FirstOrDefault(c => c.Type == "DateOfJoing").Value);
+                spendingTimeWithCompany = DateTime.Today.Year - date.Year;
+            }
+
+            if (spendingTimeWithCompany > 5)
+            {
+                return new string[] { "High Time1", "High Time2", "High Time3", "High Time4", "High Time5" };
+            }
+            else
+            {
+                return new string[] { "value1", "value2", "value3", "value4", "value5" };
+            }
+        }
+
+        [HttpGet]
+        [Route("Get5")]
+        [Authorize(Policy = "RequireAdministratorRole")]
+        public ActionResult<IEnumerable<string>> Get5()
+        {
+            var currentUser = HttpContext.User;
+            int spendingTimeWithCompany = 0;
+
+            if (currentUser.HasClaim(c => c.Type == "DateOfJoing"))
+            {
+                DateTime date = DateTime.Parse(currentUser.Claims.FirstOrDefault(c => c.Type == "DateOfJoing").Value);
+                spendingTimeWithCompany = DateTime.Today.Year - date.Year;
+            }
+
+            if (spendingTimeWithCompany > 5)
+            {
+                return new string[] { "High Time1", "High Time2", "High Time3", "High Time4", "High Time5" };
+            }
+            else
+            {
+                return new string[] { "value1", "value2", "value3", "value4", "value5" };
+            }
+        }
+
+        [HttpGet]
+        [Route("Get6")]
+        [Authorize(Policy = "RequireDBARole")]
+        public ActionResult<IEnumerable<string>> Get6()
         {
             var currentUser = HttpContext.User;
             int spendingTimeWithCompany = 0;
